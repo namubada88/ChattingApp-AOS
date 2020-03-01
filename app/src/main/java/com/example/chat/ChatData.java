6142e241809431997d40f6d559fe4246eb61eed4
mw <augustman8808@gmail.com>
2020-03-02 07:01:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/chat/ChatData.java
@@ -0,0 +1,31 @@
+package com.example.chat;
+
+public class ChatData {
+    private String msg;
+    private String nickname;
+    private String fcmToken;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getFcmToken() {
+        return fcmToken;
+    }
+
+    public void setFcmToken(String fcmToken) {
+        this.fcmToken = fcmToken;
+    }
+}
